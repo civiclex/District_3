48f99ed1835048dd904804dd50c2382796264542
jessibreen <jessibreen@gmail.com>
2020-05-26 09:58:23 -0400

    removed unused code

--- js/map.js
+++ js/map.js
@@ -1,19 +1,11 @@
-//create the empty layers
-var district = L.layerGroup();
-
- //Initialize the leaflet map
+//Initialize the leaflet map
  var map = L.map('map', {
     center: [38.0147,-84.483],
     zoom: 11,
     zoomControl: true,
     dragging: true,
 });
-
-// Create additional map panes to fix layering issue
-//map.createPane("pane200").style.zIndex = 200; // tile pane
-//map.createPane("pane450").style.zIndex = 450; // between overlays and shadows
-//map.createPane("pane600").style.zIndex = 600; // marker pane
-       
+     
 //Create the baselayer and add it to the map
 var layer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -27,10 +19,6 @@ map.addLayer(layer);
 //Create the district boundaries layer from the geojson
 $.getJSON("data/Dist3.geojson", function(data) {
     districtLayer = L.geoJson(data, {
-    /* onEachFeature: function (feature, layer){
-        district.addLayer(layer), 
-        layer.bindPopup('District '+feature.properties.DISTRICT)},
-    // pane: "pane450", */
     weight: 1,
     opacity: 1,
     color: '#fa0000',
