cdfdd40afe563191948f95fbc067c2ea59ad4f45
jessibreen <jessibreen@gmail.com>
2020-05-28 10:29:17 -0400

    setting boundaries to districtLayer

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
-//Initialize the leaflet map
+/* //Initialize the leaflet map
  var map = L.map('map', {
     center: [38.0147,-84.483],
     zoom: 11,
     zoomControl: true,
     dragging: true,
-});
+}); */
      
-//Create the baselayer and add it to the map
-var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+//Create the baselayer
+var baseLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	maxZoom: 19
+	//maxZoom: 19
 });
-map.addLayer(layer);
+//map.addLayer(layer);
 
 //Create the district boundaries layer from the geojson
 $.getJSON("data/Dist3.geojson", function(data) {
@@ -20,5 +20,16 @@ $.getJSON("data/Dist3.geojson", function(data) {
     stroke: false,
     fillColor: '#ef6e4e',
     fillOpacity: 0.1
-}).addTo(map);
+})//.addTo(map);
 });
+
+//Initialize the leaflet map and add layers
+var map = L.map('map', {
+    center: [38.0147,-84.483],
+    zoom: 11,
+    zoomControl: true,
+    dragging: true,
+}).fitBounds(districtLayer.getBounds());
+baseLayer.addTo(map);
+districtLayer.addTo(map);
+})
